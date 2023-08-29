d8fb1c628c4ef6ae25adfc631b7224bcb492f5eb
Bernardo Di Miceli <bdm.444@gmail.com>
2023-08-29 11:10:48 +0200

    non funzionante

--- src/app.mjs
+++ src/app.mjs
@@ -1,13 +1,7 @@
-export const lambdaHandler = async (event, context) => {
-    try {
-        return {
-            'statusCode': 200,
-            'body': JSON.stringify({
-                message: 'hello world',
-            })
-        }
-    } catch (err) {
-        console.log(err);
-        return err;
-    }
+console.log('Loading function');
+
+export const handler = async (event, context) => {
+    const date = new Date();
+    console.log(`Sono le ${date.getHours()} e ${date.getMinutes()} minuti`);
+    return true;
 };
